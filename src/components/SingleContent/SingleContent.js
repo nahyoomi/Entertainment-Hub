2bf61b066c791eeaf2d5adc184153df214dbfc94
Nahomi <nahyomi2000@gmail.com>
2022-10-16 18:08:28 -0500

    :lipstick:(Add)Created pagination component and added styles to trending

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,3 +1,4 @@
+import { Badge } from '@mui/material';
 import React from 'react'
 import { img_300, unavailable } from '../config/config';
 import './SingleContent.css';
@@ -13,6 +14,7 @@ const SingleContent =({
 }) => {
   return (
     <div className='media' >
+      <Badge badgeContent={vote_average} color={vote_average > 6 ? 'primary' : 'secondary'} />
         <img className='poster' src={poster ? `${img_300}/${poster}`:unavailable} alt={title} />
         <b className='title'>{title}</b>
         <span className="subtitle">
