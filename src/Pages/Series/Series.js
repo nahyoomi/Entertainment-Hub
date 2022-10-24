a88e3102f7c3134d02280f3935c25a816d5aab78
Nahomi <nahyomi2000@gmail.com>
2022-10-24 13:50:51 -0500

    :sparkles:(Feat)Tv series page with filters

--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import  SingleContent  from '../../components/SingleContent/SingleContent';
+import Genres from '../../components/Genres';
+import CustomPagination from '../../components/Pagination/CustomPagination';
+import useGenres from '../../hooks/useGenre';
 
 function Series() {
+  const [ page, setPage ] = useState(1);
+  const [ content, setContent ] = useState([]);
+  const [numOfPages, setnumOfPages] = useState();
+  const [ selectedGenres, setSelectedGenres ] = useState([]);
+  const [ genres, setGenres ] = useState();
+  const genreforURL = useGenres(selectedGenres);
+
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+    `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_APPI_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+    );
+    setContent(data.results);
+    setnumOfPages(data.total_pages); 
+  };
+
+  const numDecimal = (num) => {
+    const voteDecimal = num.toFixed(1);
+    return voteDecimal;
+ }
+
+  useEffect(() => {
+    fetchMovies();
+  }, [genreforURL, page]);
+  
   return (
     <div>
-        <span className='pageTitle'>Series</span>
+        <span className='pageTitle'>TV Series</span>
+        <Genres 
+          type='tv'
+          selectedGenres={selectedGenres}
+          setSelectedGenres={setSelectedGenres}
+          genres={genres}
+          setGenres={setGenres}
+          setPage={setPage}
+        />
+        <div className='trending'>
+          {content && content.map((c) => (
+            <SingleContent 
+              key={c.id} 
+              id={c.id} 
+              poster={c.poster_path} 
+              title={c.title || c.name} 
+              date={c.first_air_date || c.release_date}
+              media_type='tv'
+              vote_average={numDecimal(c.vote_average)}  
+            />
+          ))}  
+        </div>
+        {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages}/>
+        )}
     </div>
   )
 }
