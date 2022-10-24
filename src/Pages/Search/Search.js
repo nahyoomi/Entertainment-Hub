e7f8fee73c1622873b09fb1f7e720de6cd8f65c9
Nahomi <nahyomi2000@gmail.com>
2022-10-24 15:33:46 -0500

    :sparkles:(Feat)Added search bar to locate movies or tv series

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import axios from 'axios';
+import { ButtonBase, Tab, Tabs, TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { createTheme, Pagination, ThemeProvider } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import SingleContent from "../../components/SingleContent/SingleContent";
+
 
 function Search() {
+  const [ type, setType ] = useState(0);
+  const [ page, setPage ] = useState(1);
+  const [ searchText, setSearchText ] = useState('');
+  const [ content, setContent ] = useState();
+  const [ numOfPages, setNumOfPages ] = useState();
+
+  const darkTheme = createTheme({
+    palette: {
+        text: 'divider'
+    }
+});
+
+const fetchSearch = async () => {
+ const { data } = await axios.get(
+  `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${process.env.REACT_APP_APPI_KEY}&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+ );
+
+ setContent(data.results);
+ setNumOfPages(data.total_pages);
+ console.log(data)
+};
+
+useEffect(() => {
+  window.scroll(0, 0);
+  fetchSearch();
+}, [ type, page ])
+
   return (
     <div>
-        <span className='pageTitle'>Search</span>
+      <ThemeProvider theme={darkTheme}>
+        <div style={{display:'flex', margin: '15px 0'}}>
+          <TextField
+            style={{flex: 1}}
+            className='searchBox'
+            label='Search'
+            variant='filled'
+            onChange={(e) => setSearchText(e.target.value)}
+          /> 
+          <ButtonBase 
+            variant='contained' 
+            style={{marginLeft: 10}} 
+            onClick={fetchSearch}> 
+            <SearchIcon /> 
+          </ButtonBase>
+        </div>
+
+        <Tabs 
+          value={type} 
+          indicatorColor='primary' 
+          textColor='primary'
+          onChange={(event, newValue) => {
+            setType(newValue);
+            setPage(1);
+          }}
+          style={{paddingBottom: 5}}
+          > 
+          <Tab style={{ width: "50%" }} label="Search Movies" />
+          <Tab style={{ width: "50%" }} label="Search TV Series" />
+        </Tabs>
+
+      </ThemeProvider>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={type ? "tv" : "movie"}
+              vote_average={c.vote_average}
+            />
+          ))}
+        {searchText &&
+          !content &&
+          (type ? <h2>No Series Found</h2> : <h2>No Movies Found</h2>)}
+      </div>
+      {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+      )}
     </div>
   )
 }
